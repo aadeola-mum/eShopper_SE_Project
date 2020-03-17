5a559d03c192eb543b4d7bc308eceb0f6a055e89
ab3656 <ameetbht@gmail.com>
2020-03-17 12:21:29 -0500

    Implemented pagination, search by keyword and search by catergory

--- src/main/java/cs425/team4/eshopper/services/ProductService.java
+++ src/main/java/cs425/team4/eshopper/services/ProductService.java
@@ -2,6 +2,11 @@ package cs425.team4.eshopper.services;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import cs425.team4.eshopper.models.Order;
 import cs425.team4.eshopper.models.Product;
 
 public interface ProductService {
@@ -9,6 +14,8 @@ public interface ProductService {
 	public Optional<Product> get(long id);
 	public Product save(Product product);
 	public boolean delete(long id);
-	public Iterable<Product> search(String searchText);
+	public Page<Product> findAllbyPageAndSize(int page, int size);
+	public Page<Product> search(int page, int size, String keyword);
+	public Page<Product> searchByCategory(int page, int size, String category);
 	
 }
